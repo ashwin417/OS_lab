658ed076c671b04265ac80c64d90e010011d8933
R Ashwin <77284634+ashwin417@users.noreply.github.com>
2022-06-20 09:26:02 +0530

    Sorting

--- FCFS.C
+++ FCFS.C
@@ -1,7 +1,7 @@
 #include<stdio.h>
 int main()
 {
-	int i,burst=0, n;
+	int i,j,burst=0, n;
 	printf("Enter the number of processes : ");
 	scanf("%d",&n);
 	int prc[n], Bt[n], At[n];
@@ -9,10 +9,33 @@ int main()
 	for(i=0;i<n;i++)
 		scanf("%d%d%d",&prc[i],&At[i],&Bt[i]);
 
+//Sorting according to burst time (selection sort)
+	int t,pos;
+	for(i=0;i<n;i++)
+	{
+	pos = i;
+		for(j=i+1;j<n;j++)
+		{
+			if(At[j]<At[pos])
+				pos = j;
+		}
+//Swapping 
+	t=Bt[i];
+	Bt[i]=Bt[pos];
+	Bt[pos]=t;
+	
+	t=At[i];
+	At[i]=At[pos];
+	At[pos]=t;
+	
+	t=prc[i];
+	prc[i]=prc[pos];
+	prc[pos]=t;
+	}		
 // Waiting time calculation
 	int Wt[n];
 	for(int i=0;i<n;i++)
-	{
+	{	
 	if(i==0)
 	{
 		Wt[i]=At[i];
@@ -33,13 +56,14 @@ int main()
 		Ta[i]=temp-At[i];
 	}
 	
+//Displaying the table with Waiting time and turn around time
 	printf("\nPno\tBT\tAT\tWT\tTA");
 	for(i=0;i<n;i++)
 	{
 		printf("\n%d\t%d\t%d\t%d\t%d\n",prc[i],Bt[i],At[i],Wt[i],Ta[i]);
 	}
 	
-	
+//Finding Average waiting time	
 	int SumWT=0;
 	for(i=0;i<n;i++)
 	{
@@ -47,6 +71,7 @@ int main()
 	}
 	float AvgWT=(float)SumWT/(float)n;
 	
+//Finding Average Turn Around time
 	int SumTA=0;
 	for(i=0;i<n;i++)
 	{
@@ -55,6 +80,7 @@ int main()
 	float AvgTa=(float)SumTA/(float)n;
 	
 	printf("\nAverage Waiting Time = %f milliseconds\nAverage Turnaround time = %f milliseconds\n",AvgWT,AvgTa);
+	
 	return 0;
 }
 
