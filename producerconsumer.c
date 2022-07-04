121bcb7830e72a18b6b4f73f687e7bf77eff40dd
R Ashwin <77284634+ashwin417@users.noreply.github.com>
2022-07-04 11:35:16 +0530

    Update producerconsumer.c

--- producerconsumer.c
+++ producerconsumer.c
@@ -4,7 +4,7 @@ int buffer[10];
 void producer()
 {
 	int next_produced=0;
-	while( counter == Buffer_size);
+	//while( counter == Buffer_size);
 	buffer[in] = ++next_produced;
 	in = (in + 1) % Buffer_size;
 	counter++;
@@ -14,7 +14,7 @@ void producer()
 void consumer()
 {
 	int next_consumed;
-	while( counter == 0 );
+	//while( counter == 0 );
 	next_consumed = buffer[out];
 	out = (out + 1) % Buffer_size;
 	counter--;
@@ -25,26 +25,26 @@ int main()
 int ch,k=0;
 while(ch!=3)
 	{
-	printf("\n1. Producer\n2. Consumer\n3. Exit \nEnter option: ");
+	printf("\n1. Producer 2. Consumer 3. Exit \nEnter option: ");
 	scanf("%d",&ch);
 	switch(ch)
 	{
 		case 1 : if(counter == Buffer_size)
 				{
-				printf("Buffer Full\n");
+				printf("\nBuffer Full\n");
 				}
 				else
 				{ 
-				 printf("Producer produces the item %d\n",++k);
+				 printf("\nProducer produces the item %d\n",++k);
 				 producer();
 				} 
 				break;
 		case 2 :{
 			if(counter == 0)
 				{
-				printf("Buffer empty\n");}
+				printf("\nBuffer empty\n");}
 			else{ 
-				printf("Consumer Consumed the item %d\n",k--); consumer();
+				printf("\nConsumer Consumed the item %d\n",k--); consumer();
 				}
 			}
 			break;
