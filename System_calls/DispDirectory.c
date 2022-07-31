3f042764b353cfebb99d25961cc5f15798300d04
R Ashwin <77284634+ashwin417@users.noreply.github.com>
2022-07-31 10:06:00 +0530

    Update DispDirectory.c

--- System_calls/DispDirectory.c
+++ System_calls/DispDirectory.c
@@ -2,17 +2,21 @@
 #include<dirent.h>
 int main()
 {
-  struct dirent *de;
-  printf("Enter the directory name along with its path to list it's contents : ");
-  char dirpath[50];
-  scanf("%s",dirpath);
-  DIR *dr = opendir(dirpath);
-  if(dr == NULL)
-    printf("\n\tCouldnot open current directory !!!");
-    return 0;
-  printf("\nDirectories in %s are : \n",dirpath);
-  while((de = readdir(dr))!= NULL)
-    printf("\t%s\n\n",de->dname);
-  closedir(dir);
-  return 0;
+	struct dirent *de;
+	printf("Enter the directory name along with its path to list it's contents : ");
+	char dirpath[50];
+	scanf("%s",dirpath);
+	DIR *dr = opendir(dirpath);
+	if(dr == NULL)
+		{
+			printf("\n\tCouldnot open current directory !!!");
+			return 0;
+		}
+	printf("\nDirectories in %s are : \n",dirpath);
+	while((de = readdir(dr))!= NULL)
+  {
+		printf("\t%s\n\n",de->d_name);
+  }
+	closedir(dr);
+	return 0;
 }
