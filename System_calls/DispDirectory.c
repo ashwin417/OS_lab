76b4a598044a2811fc27a960914ef0434faab464
R Ashwin <77284634+ashwin417@users.noreply.github.com>
2022-07-31 09:56:01 +0530

    Create DispDirectory.c

---
+++ System_calls/DispDirectory.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<dirent.h>
+int main()
+{
+  struct dirent *de;
+  printf("Enter the directory name along with its path to list it's contents : ");
+  char dirpath[50];
+  scanf("%s",dirpath);
+  DIR *dr = opendir(dirpath);
+  if(dr == NULL)
+    printf("\n\tCouldnot open current directory !!!");
+    return 0;
+  printf("\nDirectories in %s are : \n",dirpath);
+  while((de = readdir(dr))!= NULL)
+    printf("\t%s\n\n",de->dname);
+  closedir(dir);
+  return 0;
+}
