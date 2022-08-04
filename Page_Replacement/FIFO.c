f6f464a355f090ace7bd099588116a5904b81444
R Ashwin <77284634+ashwin417@users.noreply.github.com>
2022-08-04 09:55:51 +0530

    Create FIFO.c

---
+++ Page_Replacement/FIFO.c
@@ -0,0 +1,54 @@
+#include<stdio.h>
+void FIFO(int prs[], int n, int frame);
+
+void main()
+{
+	int n,i,frame;
+	printf("Enter the Length of sequence : ");
+	scanf("%d",&n);
+	int prs[n];
+	printf("Enter the Sequence : ");
+	for(i=0;i<n;i++)
+		scanf("%d",&prs[i]);
+	printf("Enter the number of frame : ");
+	scanf("%d",&frame);
+	FIFO(prs,n,frame); 
+}
+	
+void FIFO(int prs[], int n, int frame)
+	{
+		int pagefault = 0, hit=0,i,j,s;
+		int temp[frame];
+		for(i= 0; i< frame; i++)
+		{
+			temp[i] = -1;
+		}
+		for(i= 0; i< n; i++)
+		{
+			s = 0;
+			for(j = 0; j < frame; j++)
+			{
+				if(prs[i] == temp[j])
+				{
+					s++;
+					pagefault--;
+				}
+			}     
+			pagefault++;
+			if((pagefault <= frame) && (s == 0))
+			{
+				temp[i] = prs[i];
+			}   
+			else if(s == 0)
+			{
+				temp[(pagefault - 1) % frame] = prs[i];
+			}
+			printf("\n");
+			for(j = 0; j < frame; j++)
+			{ 
+					printf("%d\t", temp[j]);
+			}
+		} 
+		printf("\nTotal Page Faults:\t%d\n", pagefault);
+	}
+		
