989238d3c53dbbf92331596095e6f4d22c50cab8
R Ashwin <77284634+ashwin417@users.noreply.github.com>
2022-08-04 12:31:12 +0530

    Create LFU.c

---
+++ Page_Replacement/LFU.c
@@ -0,0 +1,77 @@
+#include<stdio.h>
+void LFU(int prs[], int n, int frame);
+void print(int [],int);
+void main()
+{
+	int n,i,frame;
+	printf("Enter the Length of sequence : ");
+	scanf("%d",&n);
+	int prs[n];
+	printf("Enter the Sequence : ");
+	for(i=0;i<n;i++)
+		scanf("%d",&prs[i]);
+	printf("Enter the number of frame : ");
+	scanf("%d",&frame);
+	LFU(prs,n,frame); 
+}
+
+void LFU(int pr[], int n, int f)
+{
+	int pagefault = 0, i,j,freq[n],minimum,index,flag=0,frames[n],pos=0;
+	printf("\nValues\t");
+	for(i=1;i<=f;i++)
+		printf("Frame%d\t",i);
+	printf("\n");
+	for(i=0;i<n;i++)
+		frames[i]=-1;
+	for(i=0;i<n;i++)
+	{
+		printf("\n");
+		printf("%d\t",pr[i]);
+		flag=0;
+		for(j=0;j<n;j++)
+		{
+			if(pr[i]==frames[i])
+			{
+				flag = 1;
+				freq[i]++;
+				printf("\n");
+				break;
+			}
+		}
+		if(flag==0 && pagefault<f)
+		{
+			frames[pos]=pr[i];
+			freq[pos]=1;
+			pos = (pos+1)%f;
+			pagefault++;
+			print(frames,f);
+		}
+		else if(flag==0)
+		{
+			index = 0;
+			minimum = freq[0];
+			for(j=1;j<f;j++)
+			{
+				if(freq[j]<minimum)
+				{
+					index=j;
+					minimum = freq[j];
+				}	
+			}
+			frames[index]=pr[i];
+			freq[index]=1;
+			pagefault++;
+			print(frames,f);
+		}
+	}
+	printf("Total Page Faults : %d\n",pagefault);
+}
+
+void print(int frames[], int f)
+{
+	int j;
+	for(j=0;j<f;j++)
+		printf("%d\t",frames[j]);
+	printf("\n");
+}
